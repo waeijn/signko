00df31febbbae5c4499fad2a92afae8667dcfe38
John Wayne Landong <johnwaynelandong@gmail.com>
2026-09-23 22:52:19 +0800

    chore: comment out unused flutter_blue_plus import

--- app/lib/services/ble_service.dart
+++ app/lib/services/ble_service.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+// import 'package:flutter_blue_plus/flutter_blue_plus.dart'; // TODO: Uncomment when building BLE logic
 
 /// A Riverpod provider to access the BleService instance globally
 final bleServiceProvider = Provider<BleService>((ref) {
